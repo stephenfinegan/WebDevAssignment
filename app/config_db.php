234f68204282c21c068172216fb98143a898979b
stephenfinegan <stephenrooneyfinegan@gmail.com>
2016-04-27 17:25:41 +0100

    php file to connect to db

---
+++ app/config_db.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 13/04/2016
+ * Time: 16:36
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'stephen');
+define('DB_PASS', 'qwerty');
+define('DB_NAME', 'assignment1_cvs');
